f4d22c41e69b07ab9da64de52e03b9a4f906e2f3
Linus Honer <lhon2701@gmail.com>
2024-06-26 14:20:20 -0500

    Initial Avalonia Commit

---
+++ MinecraftModding/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace MinecraftModding.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public ICommand BuyMusicCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        BuyMusicCommand = ReactiveCommand.Create(() =>
+        {
+            // Code here will be executed when the button is clicked.
+        });
+    }
+}
---
+++ MinecraftModding/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace MinecraftModding.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
